5858991cb5a81cc27efd033e2cff34b5d8d5bb76
Anastassios Nanos <ananos@nubificus.co.uk>
2025-03-19 18:59:03 +0000

    feat(logo): Update with athk8s logo
    Signed-off-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- main.c
+++ main.c
@@ -53,7 +53,7 @@ static const char reply[] =
     "<style>table {border-collapse: collapse; width: 50%%;}th, td {border: 1px solid #dddddd; text-align: left; padding: 8px;}th {background-color: #f2f2f2;}</style>"
     "</head>"
     "<body>"
-    "<h1> Hello <img src=\"%s\" alt=\"HiPEAC Logo\" height=\"100px\" /></h1>"
+    "<h1> Hello <img src=\"%s\" alt=\"Ath K8s Logo\" height=\"100px\" /></h1>"
     "<h2> RuntimeClass</h2>"
     "<img src=\"%s\" alt=\"Runtime Class\" height=\"100px\" />"
     "<h1>Request Headers</h1>"
@@ -349,7 +349,7 @@ int main(int argc __attribute__((unused)),
 
 		const char *imageURL = determineImageURL(hostname);
 		const char *image2 = "https://s3.nbfc.io/hypervisor-logos/nubis-logo-scaled.png";
-		const char *image1 = "https://s3.nbfc.io/hypervisor-logos/hipeac.png";
+		const char *image1 = "https://s3.nbfc.io/hypervisor-logos/athk8s.png";
 
 		// Format the reply using snprintf
 		int reply_len = snprintf(final_reply, sizeof(final_reply), reply, image1, imageURL, headers_html, image2);
